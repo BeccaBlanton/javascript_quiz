4d1519e5d2d2876e5cda2c8099bee9572002bf3b
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-16 21:58:13 -0700

    added in initial pseudo coding mark up

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Code Quiz
+Quiz starts when pressing a button
+button starts a timer 
+first question appears on screen
+question contains text and multiple buttons for multiple choice
+once one button is pressed page refreshes to next question
+if wrong button is pressed, then time will be subtracted from total
+after set amount of questions or timer goes off which ever comes first
+end quiz if either occur
+give end game statement
+form that allows to enter intials
+initials and score posted onto page
+*/
