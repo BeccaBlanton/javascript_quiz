253b37912903c89608a4cc11e7f54481a6293110
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-18 10:21:54 -0700

    5 minute timer that starts when pressing start button for quiz. Displayed in minutes and seconds in jumbotron header

--- script.js
+++ script.js
@@ -19,4 +19,53 @@ var results = document.getElementById('results');
 var submitBtn = document.getElementById('submit');
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
+//Timer Variables
+var totalSeconds = 300;
+var secondsElapsed = 0;
+var interval;
+console.log(totalSeconds)
+console.log(secondsElapsed)
+//Timer to start when user clicks Start for the quiz
+function startTimer() {
+    if (totalSeconds > 0) {
+        interval = setInterval(function() {
+          secondsElapsed++;
+          renderTime();
+          console.log(renderTime)
+        }, 1000);
+    } 
+  }
+  function renderTime() {
+    // When renderTime is called it sets the textContent for the timer html...
+    minutesDisplay.textContent = getFormattedMinutes();
+    secondsDisplay.textContent = getFormattedSeconds();
+    if (secondsElapsed >= totalSeconds) {
+       alert("Time's Up!")
+        stopTimer();
+      }
+    }
+    function getFormattedMinutes() {
+        var secondsLeft = totalSeconds - secondsElapsed;
+        var minutesLeft = Math.floor(secondsLeft / 60);
+        var formattedMinutes;
+        if (minutesLeft < 10) {
+          formattedMinutes = "0" + minutesLeft;
+        } else {
+          formattedMinutes = minutesLeft;
+        }
+        console.log(secondsLeft)
+    console.log(minutesLeft)
+        return formattedMinutes;
+      }
+    function getFormattedSeconds() {
+        var secondsLeft = (totalSeconds - secondsElapsed) % 60;
+        var formattedSeconds;
+        if (secondsLeft < 10) {
+          formattedSeconds = "0" + secondsLeft;
+        } else {
+          formattedSeconds = secondsLeft;
+        }
+        return formattedSeconds;
+      }
 
+startBtn.addEventListener("click", startTimer);
