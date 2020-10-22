0e5285a395c377e54b4b039ebd0fba7d20d2fa47
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-22 07:43:10 -0700

    restarted functions for running quiz. made new pseudo coding, new base HTML and CSS. Starting point #2

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ give end game statement
 form that allows to enter intials
 initials and score posted onto page
 */
-//Starting elements to pull from HTML for main elements and timer function
+
 var startBtn = document.getElementById('startBtn')
 var submitBtn = document.getElementById('submitBtn');
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
-
+//GLOBALS
+const currentQuestion =''
+const score = 0
+//press start and quiz begins and time starts
+startBtn.addEventListener('click', initQuiz)
+startBtn.addEventListener('click', startTimer)
 //Timer Variables
 var totalSeconds = 300;
 var secondsElapsed = 0;
@@ -60,19 +65,9 @@ function startTimer() {
         return formattedSeconds;
       }
 
-startBtn.addEventListener("click", startTimer);
 
-
-
-var ul = document.getElementById('ul')
-var nextButton = document.getElementById('btnNext');
-var quizbox = document.getElementById('questionBox')
-var opt1 = document.getElementById('opt1')
-var opt2 = document.getElementById('opt2')
-var opt3 = document.getElementById('opt3')
-var opt4 = document.getElementById('opt4')
-
-var app={
+//array of questions, choices and answers
+var questionArr={
     questions: [
         {
           question: "What operator is used to assign a value to a Variable?",
@@ -100,62 +95,60 @@ var app={
           correctAnswer: 2
       }
       ],
-        index:0,
-        load:function(){
-            if(this.index<=this.questions.length-1){
-                quizbox.innerHTML=this.index+1 + ". " +this.questions[this.index].question;
-                opt1.innerHTML=this.questions[this.index].choices[0];
-                opt2.innerHTML=this.questions[this.index].choices[1];
-                opt3.innerHTML=this.questions[this.index].choices[2];
-                opt4.innerHTML=this.questions[this.index].choices[3];
-            }
-            else {
-                quizbox.innerHTML="Quiz Completed!";
-                ul.style.display="none";
-                nextButton.style.display="none";
-            }
-        },
-        next: function(){
-            this.index++;
-            this.load();
-        },
-        check: function(ele){
-            var id=ele.id.split('');
-            if(id[id.length-1]==this.questions[this.index].correctAnswer){
-                this.score++;
-                ele.className="correct";
-                this.scoreCard();
-            }
-            else{
-                ele.className="wrong";
-            }
-        },
-        preventClick:function(){
-            for(let i=0; i<ul.children.length; i++){
-                ul.children[i].style.pointerEvents="none";
-            }
-        },
-        allowClick:function(){
-            for(let i=0; i<ul.children.length; i++){
-                ul.children[i].style.pointerEvents="auto";
-                ul.children[i].className=''
-            }
-        },
-        score:0,
-        scoreCard:function(){
-            scoreCard.innerHTML= this.score + "/" + this.questions.length;
-        }
+    }
+
+
+
+//function for start of quiz
+function initQuiz(){
+//init globals
+currentQuestion = 0
+starttimer()
+loadNextQuestion()
+}
+    function timer(){
+console.log(timer.started)
+    }
+//first question appears on screen with multiple buttons
+//if correct, get one point
+//else incorrect answer. and time deducted from timer
+function checkAnswers(){
+if() 
+    currentQuestion++
+
+else()
+loadNextQuestion(currentQuestion)
 }
 
-window.load=app.load();
+//press any option and next question loads
 
-function button(ele){
-    app.check(ele);
-    app.preventClick();
+function loadNextQuestion(currentQuestionIndex){
+    console.log(questions[currentQuestionIndex])
+//load up question
+//continue until all questions
+//end quiz when time runs out or al questions are answered
+}
+//continue until all questions
+//end quiz when time runs out or al questions are answered
+function saveData(){
+//give out alert end message
+//form pops up to fill in initials and put in score
+//score loads onto page.
 }
 
-function next(){
-    app.next();
-    app.allowClick();
+
+//Ui logic
+//attach event listeners to all the options
+
+
+//button for init game
+document.querySelector('#start').addEventListener('click', initQuiz)
+
+function(e){
+    if(e.target.classlist.contains('option')){
+        return
+    }
+    //do the thing
 }
+//button for each answer, runs check answer
 
