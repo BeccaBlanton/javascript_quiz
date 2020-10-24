d55daeb4573710ece03e68855a8a2e955f3eff38
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-23 21:44:00 -0700

    edited styling in CSS. changing color schemes

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-/*
-Code Quiz
-first question appears on screen
-question contains text and multiple buttons for multiple choice
-once one button is pressed page refreshes to next question
-if wrong button is pressed, then time will be subtracted from total
-after set amount of questions or timer goes off which ever comes first
-end quiz if either occur
-give end game statement
+
+/*if wrong button is pressed, then time will be subtracted from total
 form that allows to enter intials
 initials and score posted onto page
 */
-
+//All Global Variables getting element from HTML
+var main = document.querySelector("main")
 var startBtn = document.getElementById('startBtn')
 var submitBtn = document.getElementById('submitBtn');
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
 var quizBox= document.querySelector('.quizbox')
-var results= document.querySelector(".resultsbox")
-//GLOBALS
-var currentQuestion =''
+var scoreForm= document.querySelector("#scoreform")
+var quizQuestion = document.querySelector("#quizquestion")
+var choice1 = document.querySelector("#btn1")
+var choice2 = document.querySelector("#btn2")
+var choice3 = document.querySelector("#btn3")
+var choice4 = document.querySelector("#btn4")
+var i = 0
 var score = 0
-
-
 //Timer Variables
-var totalSeconds = 300;
+var totalSeconds = 180;
 var secondsElapsed = 0;
 var interval;
 
-//array of questions, choices and answers
-
+//array of questions, choices and answers for Quiz
     var questionArr=[
         {
           question: "What operator is used to assign a value to a Variable?",
@@ -62,14 +57,12 @@ startBtn.addEventListener('click', initQuiz)
 
 //function for start of quiz
 function initQuiz(){
-//init globals
-currentQuestion = 0
+//init globals: Timer starts with first function called and First question appears with 2nd function
 startTimer()
-loadNextQuestion()
+loadQuestion()
+main.setAttribute("style", "display:block")
 }
-
-
-    //Timer to start when user clicks Start for the quiz
+ //Timer to start when user clicks Start for the quiz
 function startTimer() {
   if (totalSeconds > 0) {
       interval = setInterval(function() {
@@ -78,14 +71,15 @@ function startTimer() {
       }, 1000);
   } 
 }
+// When renderTime is called it sets the textContent for the timer html...
 function renderTime() {
-  // When renderTime is called it sets the textContent for the timer html...
   minutesDisplay.textContent = getFormattedMinutes();
   secondsDisplay.textContent = getFormattedSeconds();
   if (secondsElapsed >= totalSeconds) {
       stopTimer();
     }
   }
+  //formatting to appear on page as minutes and seconds
   function getFormattedMinutes() {
       var secondsLeft = totalSeconds - secondsElapsed;
       var minutesLeft = Math.floor(secondsLeft / 60);
@@ -95,7 +89,6 @@ function renderTime() {
       } else {
         formattedMinutes = minutesLeft;
       }
-      
       return formattedMinutes;
     }
   function getFormattedSeconds() {
@@ -108,82 +101,75 @@ function renderTime() {
       }
       return formattedSeconds;
     }
-
+//clears once reaches 0, submit button pressed or all questions are answered
     function stopTimer(){
       clearInterval(interval)
       secondsElapsed = 0;
     renderTime();
     }
 
+//Start of Quiz functions loads questions and choices from array to quiz box
+function loadQuestion(){
+  console.log(i)
+    quizQuestion.textContent = questionArr[i].question;
+    choice1.textContent = questionArr[i].choices[0]
+    choice2.textContent = questionArr[i].choices[1]
+    choice3.textContent = questionArr[i].choices[2]
+    choice4.textContent = questionArr[i].choices[3]
+  }
+
 //function runs after one of choices buttons is clicked, then moves to next question
-//time not subtracting if wrong
-function checkAnswers(){
+//time not subtracting if wrong(need to fix)
+function checkAnswers(event){
+  console.log(i)
    var selection = event.target
    if(selection.matches('.btn-primary')){
        var selectedItem = selection.id
        console.log(selectedItem)
        //checks if btn pressed matches correctAnswer in array
-       if(selectedItem === questionArr[currentQuestion].correctAnswer){
+       if(selectedItem === questionArr[i].correctAnswer){
        score++}
       console.log(score)
    } //if clicked other btn, will deduct time
        else{
-           totalSeconds=totalSeconds - 30
+           totalSeconds - 30
+           console.log(totalSeconds)
        }
-  loadNextQuestion()
+       if(i < 5){
+        loadQuestion()
+       } else {
+         saveData()
+       }
+       //increments i to load the next question in loadQuestion function
+       i++
   }
-//press any option and next question loads
-//currently loads all questions
-function loadNextQuestion(){
-  var i=0
-    var currentQuestion = document.createElement("ol")
-    currentQuestion.textContent = questionArr[i].question;
-    quizBox.appendChild(currentQuestion)
-    var choice1 = document.createElement("li")
-    var choice2 = document.createElement("li")
-    var choice3 = document.createElement("li")
-    var choice4 = document.createElement("li")
-    choice1.textContent = questionArr[i].choices[0]
-    choice2.textContent = questionArr[i].choices[1]
-    choice3.textContent = questionArr[i].choices[2]
-    choice4.textContent = questionArr[i].choices[3]
-    currentQuestion.appendChild(choice1);
-    currentQuestion.appendChild(choice2);
-    currentQuestion.appendChild(choice3);
-    currentQuestion.appendChild(choice4);
-    choice1.setAttribute("class", "btn btn-primary")
-    choice1.setAttribute("id", "btn1")
-    choice2.setAttribute("class", "btn btn-primary")
-    choice2.setAttribute("id", "btn2")
-    choice3.setAttribute("class", "btn btn-primary")
-    choice3.setAttribute("id", "btn3")
-    choice4.setAttribute("class", "btn btn-primary")
-    choice4.setAttribute("id", "btn4")
-    document.querySelector('#btn1').onclick = checkAnswers
-    document.querySelector('#btn2').onclick = checkAnswers
-    document.querySelector('#btn3').onclick = checkAnswers
-    document.querySelector('#btn4').onclick = checkAnswers
-
-    console.log(currentQuestion)
-  }
-//end quiz when time runs out or al questions are answered
+ //once submit button is pressed or last question answered Alerts Score
 function saveData(){
   alert("Thanks for playing!! you got " + score +" out of 6 correct");
   stopTimer();
   //removes questions from screen
   quizBox.remove()
+  //hides submit button
+  submitBtn.setAttribute("style", "display: none")
+
   //to load an input form to enter name, then enter score
   var initialsForm = document.createElement("input");
-  results.append(initialsForm)
+  scoreForm.append(initialsForm)
   initialsForm.setAttribute('type', "text")
   initialsForm.setAttribute('placeholder', "name")
+  var addNameBtn = document.createElement("button")
+  addNameBtn.setAttribute('id',"addbtn")
+  addNameBtn.textContent = "Add Initials"
   var finalScore = document.createElement('p');
-  results.appendChild(finalScore)
+  scoreForm.appendChild(finalScore)
   finalScore.textContent = score
+
 }
-//Ui logic
-//attach event listeners to all the options
-//button for init game and then to submit and saveData
+//event listeners for all the buttons
 document.querySelector('#startBtn').addEventListener('click', initQuiz)
 document.querySelector('#submitBtn').addEventListener('click', saveData)
+document.querySelector('#btn1').addEventListener('click', checkAnswers)
+document.querySelector('#btn2').addEventListener('click', checkAnswers)
+document.querySelector('#btn3').addEventListener('click', checkAnswers)
+document.querySelector('#btn4').addEventListener('click', checkAnswers)
 
